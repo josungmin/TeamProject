e2db82d570014b7443801254cf8a04669debe04d
josungmin <37390159+josungmin@users.noreply.github.com>
2020-04-28 23:14:01 +0900

    적 구현

---
+++ 팀프/Assets/TeamProject/Enemy/Script/Dragon_MoveState.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Dragon_MoveState : DragonState
+{
+    private Dragon dragon;
+
+    public Transform target;
+    public Vector3 direction;
+
+    private Animator animator;
+    private EnemyInfo enemyinfo;
+
+    NavMeshAgent nav;
+
+    void DragonState.OnEnter(Dragon dragon)
+    {
+        Debug.Log("Move State");
+        this.dragon = dragon;
+        animator = dragon.GetComponent<Animator>();
+
+        nav = dragon.GetComponent<NavMeshAgent>();
+        nav.isStopped = false;
+
+        enemyinfo = dragon.GetComponent<EnemyInfo>();
+    }
+
+    void DragonState.Update()
+    {
+        animator.SetBool("isWalk", true);
+        target = GameObject.Find("Player").transform;
+        direction = (target.position - dragon.transform.position).normalized;
+
+        float distance = Vector3.Distance(target.position, dragon.transform.position);
+
+        //타겟을 향해 이동
+        nav.SetDestination(target.transform.position);
+
+        if (enemyinfo.c_HP > enemyinfo.p_HP)
+        {
+            dragon.SetState(new Dragon_DamagedState());
+        }
+
+        // 실행할것 구현
+        if (distance >= dragon.IdleRange)
+        {
+            // 다른상태로 이동
+            dragon.SetState(new Dragon_IdleState());
+        }
+        //공격범위안에 들어왔을 때
+        else if (dragon.AttackRange >= distance)
+        {
+            dragon.SetState(new Dragon_AttackState());
+        }
+    }
+
+    void DragonState.OnExit()
+    {
+        animator.SetBool("isWalk", false);
+    }
+}
---
+++ 팀프/Assets/TeamProject/Enemy/Script/EnemyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInfo : MonoBehaviour
+{
+    public float p_HP;
+    public float c_HP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
