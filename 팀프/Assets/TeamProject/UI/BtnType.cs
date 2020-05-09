0e588434de53c6e8391dbafcb226e3315a3aa7de
Parvaneh <qkrthwjd1231@naver.com>
2020-05-09 20:31:57 +0900

    mainscene_add

---
+++ 팀프/Assets/TeamProject/UI/BtnType.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BtnType : MonoBehaviour,IPointerExitHandler,IPointerEnterHandler
+{
+    public BTNType currentType;
+    public Transform buttonScale;
+    Vector3 defaultScale;
+
+    private void Start()
+    {
+        defaultScale = buttonScale.localScale;
+    }
+
+    public void OnBtnClick()
+    {
+        switch (currentType)
+        {
+            case BTNType.New:
+                Debug.Log("새 게임");
+                break;
+            case BTNType.Continue:
+                Debug.Log("이어하기");
+                break;
+            case BTNType.Sound:
+                Debug.Log("사운드");
+                break;
+            case BTNType.Quit:
+                Debug.Log("그만두기");
+                break;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonScale.localScale = defaultScale * 1.2f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonScale.localScale = defaultScale;
+    }
+
+}
